4d816dd78a20de22de5ae4cfe2ce410268fff7b4
PurveshDevOps <purveshdevops2025@gmail.com>
2025-07-02 06:06:33 +0000

    Added app.py file

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+
     return 'Hello Dosto, This is Purvesh Shapariya, welcome to DevOps Zero To Hero (Junoon  Batch 9)'
+    return 'Hello Dosto, welcome to DevOps Zero To Hero (Junoon  Batch 9)\nI’m Purvesh, an aspiring DevOps Engineer focused on optimizing workflows and delivering scalable solutions. Passionate about continuous improvement in IT.'
+ (Added app.py file)
 
 @app.route('/health')
 def health():
